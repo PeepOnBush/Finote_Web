888f88e9f9caca5cfc4dda26a1f6454ad41d5755
PeepOnBush <phandangloi07102014@gmail.com>
2025-12-02 23:28:51 +0700

    12/02/2025 thêm bảng AiLog chưa migrate

---
+++ Models/Data/AiLog.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Finote_Web.Models.Data
+{
+    public class AiLog
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; } // Foreign key to the Users table
+
+        public DateTime UsedTime { get; set; }
+
+        public string Prompt { get; set; } = string.Empty;
+
+        public string PersonSend { get; set; } = string.Empty; // The name of the user
+
+        // Navigation property to the User who performed the action
+        [ForeignKey("UserId")]
+        public virtual Users User { get; set; }
+    }
+}
